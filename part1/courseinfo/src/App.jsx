cf9f77ce068af76f438d6e8d18b11e1f13d33f7b
Ashking56 <enzosalinas57@gmail.com>
2025-04-09 10:59:09 +0200

    De nuevo resubiendo

---
+++ part1/courseinfo/src/App.jsx
@@ -0,0 +1,45 @@
+const Header = (props) => {
+  return (
+      <div>
+          <h1>{props.course}</h1>
+      </div>
+  );
+};
+
+const Content = (props) => {
+  return (
+    <div>
+      <Part part={props.parts[0].part} exercises={props.parts[0].exercises} />
+      <Part part={props.parts[1].part} exercises={props.parts[1].exercises} />
+      <Part part={props.parts[2].part} exercises={props.parts[2].exercises} />
+    </div>
+  );
+};
+
+const Total = (props) => {
+  const total = props.parts.reduce((sum, part) => sum + part.exercises, 0);
+  return (
+      <div>
+          <p>Number of exercises {total}</p>
+      </div>
+  );
+};
+
+const App = () => {
+  const course = 'Half Stack application development';
+  const parts = [
+      { part: 'Fundamentals of React', exercises: 10 },
+      { part: 'Using props to pass data', exercises: 7 },
+      { part: 'State of a component', exercises: 14 }
+  ];
+
+  return (
+      <div>
+          <Header course={course} />
+          <Content parts={parts} />
+          <Total parts={parts} />
+      </div>
+  );
+};
+
+export default App;
