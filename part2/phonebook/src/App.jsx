88d0bab57a1427f9804aa0aad1cc0253aa87abac
Ashking56 <100533870+Ashking56@users.noreply.github.com>
2025-05-02 18:23:56 +0200

    Ejerciio Agenda telefonica paso 6
    Modifica la aplicación de modo que el estado inicial de los datos se obtenga del servidor mediante la librería axios.

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,15 +1,11 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { Filter } from './components/Filter'
 import { PersonForm } from './components/PersonForm'
 import { Persons } from './components/Persons'
+import axios from 'axios'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [phoneNumber, setPhoneNumber] = useState('')
   const [searchTerm, setSearchTerm] = useState('')
@@ -24,7 +20,7 @@ const App = () => {
 
 
 
-    
+
     if (persons.some(person => person.name === newName)) {
       alert(`${newName} already exists in phonebook`);
       return;
@@ -34,6 +30,20 @@ const App = () => {
     setNewName('');
     setPhoneNumber('');
   };
+
+  const hook = () => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log('promise fulfilled');
+        setPersons(response.data)
+
+      })
+  }
+
+  useEffect(hook, [])
+
 
   const filteredPersons = persons.filter(person =>
     person.name.toLowerCase().includes(searchTerm.toLowerCase())
