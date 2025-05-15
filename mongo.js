f72f41df6504801f1110f6f5a72ec145851e493d
Ashking56 <enzosalinas57@gmail.com>
2025-05-15 10:45:58 -0400

    Nuevo haciendo ejercicio 3.12

---
+++ mongo.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+  console.log('give password as argument')
+  process.exit(1)
+}
+
+const url = `mongodb+srv://ashking:${process.argv[2]}@cluster0.dm3ze.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+
+mongoose.set('strictQuery', false)
+
+mongoose.connect(url)
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String
+})
+const Person = mongoose.model('Person', personSchema)
+
+const person = new Person({
+  name: process.argv[3],
+  number: process.argv[4]
+})
+
+if (process.argv.length == 2) {
+  Person.find({}).then(persons => {
+    console.log('phonebook:')
+    persons.forEach(person => {
+      console.log(`${person.name} ${person.number}`)
+    })
+    mongoose.connection.close()
+  })
+}
+
+person.save().then(result => {
+  console.log('person saved!')
+  mongoose.connection.close()
+})
